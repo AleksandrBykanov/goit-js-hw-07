dbc2c9ffb39cc81950a8f57d2b4beefd7dcd00d8
Aleksandr Bykanov <aleksandrbykanov1986@gmail.com>
2024-05-17 11:57:53 +0300

    make task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener("submit", callback);
+
+function callback(event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+  
+  if (email === '' || password === '') {
+    return alert("Please fill in all the fields!");
+  };
+
+  console.log(`Login: ${email}, Password: ${password}`);
+  
+  form.reset();
+}
