4aaf53c4d0fc19454983eccaf68fa30e6e5e8820
Aleksandr Bykanov <aleksandrbykanov1986@gmail.com>
2024-05-17 12:36:16 +0300

    make task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const color = document.querySelector('.color');
+
+const changeColor = () => {
+  color.textContent = body.style.backgroundColor = getRandomHexColor();
+}
+
+button.addEventListener('click', changeColor);
